e2dddc5d53c63a31811cad0d07f9377f963d98cb
XY4517CA <giussepe.caso@colaborador.elcorteingles.es>
2025-05-21 10:47:54 +0200

    chore: refactor main file to add lazy loading app and async ready router function
    Branch: chore/apply_new_project_acrhitecture

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
+import { createApp, defineAsyncComponent } from 'vue'
+import router from '@router/index'
+import pinia from '@shared/stores'
+import { i18n } from '@app/translations'
+
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// create lazy APP
+const app = createApp(defineAsyncComponent(() => import('@app/ui/layouts/App.vue')))
+app.use(router).use(i18n)
+app.config.warnHandler = () => null
 
-import App from './app/ui/layouts/App.vue'
-import router from './app/router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+router.isReady().then(() => {
+  // router.addRoute('app', recruitment)
+  app.use(pinia).mount('#app')
+})
