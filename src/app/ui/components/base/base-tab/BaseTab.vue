27e79d559e09fe0b25e7e2a5dfea1cac8d9038de
XY4517CA <giussepe.caso@colaborador.elcorteingles.es>
2025-05-25 01:05:11 +0200

    feat(BaseTag): :sparkles: add base component structure with style
    Branch: feature/components/build_tab_component

---
+++ src/app/ui/components/base/base-tab/BaseTab.vue
@@ -0,0 +1,4 @@
+<template>
+  <button class="base-tab">tab</button>
+</template>
+<style src="./BaseTab.scss" lang="scss" scoped></style>
---
+++ src/stories/base/base-tab/BaseTab.stories.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { action } from '@storybook/addon-actions'
+import BaseTab from '@components/base/base-tab/BaseTab.vue'
+import { computed } from 'vue'
+
+const meta = {
+  title: 'Base/Base Tab',
+  component: BaseTab,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+} satisfies Meta
+
+export default meta
+
+type Story = StoryObj<typeof BaseTab>
+
+const Templates: Story = {
+  render: (args: any) => ({
+    components: { BaseTab },
+    setup() {
+      const hasDefaultSLot = computed<boolean>(() => args.default !== '')
+      return { args, hasDefaultSLot }
+    },
+    template: `
+      <BaseTab
+        v-bind="args"
+        @click="click"
+      >
+        <template #default v-if="hasDefaultSLot">{{ args.default }}</template>
+      </BaseTab>
+    `,
+    methods: {
+      click: action('click'),
+    },
+  }),
+}
+
+export const Default: Story = {
+  ...Templates,
+}
