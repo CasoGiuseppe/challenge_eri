0ab59a9356f32b422b131c3220a825146174377d
XY4517CA <giussepe.caso@colaborador.elcorteingles.es>
2025-05-28 13:56:31 +0200

    fix: remove async app load
    Branch: main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp, defineAsyncComponent } from 'vue'
+import { createApp } from 'vue'
 import router from '@router/index'
 import pinia from '@shared/stores'
 import { i18n } from '@app/translations'
 import customer from '@modules/customer/presentation/router'
 import administration from '@modules/administration/presentation/router'
 import folder from '@/modules/account/presentation/router/index'
+import App from '@app/ui/layouts/App.vue'
 import '@assets/index.scss'
 
 // create lazy APP
-const app = createApp(defineAsyncComponent(() => import('@app/ui/layouts/App.vue')))
+const app = createApp(App)
 app.use(router)
 app.config.warnHandler = () => null
 
